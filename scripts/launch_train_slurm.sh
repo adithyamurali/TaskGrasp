0aa484064583b2c9b43fc5483d3be7c070cca77a
Theophile Gervet <theophile.gervet@gmail.com>
2023-03-13 23:44:15 -0400

    Add WandB logging + slurm launch file + cleanup a little

--- scripts/launch_train_slurm.sh
+++ scripts/launch_train_slurm.sh
@@ -3,8 +3,8 @@
 for split_idx in 0 1 2 3; do
   for model in "sgn" "baseline"; do
     sbatch train_1gpu_12gb.sh \
-      --cfg_file cfg/train/$model/$model_split_mode_o_split_idx_0_.yml \
-      --split_idx $split_idx \
-      --run_name $model-$split_idx
+      --cfg_file cfg/train/${model}/${model}_split_mode_o_split_idx_0_.yml \
+      --split_idx ${split_idx} \
+      --run_name ${model}-${split_idx}
   done
 done
